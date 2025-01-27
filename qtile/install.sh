b7d04bf49fe9a079ad45bb903f261037ca83ec98
Edgar <edgargarciafcb@gmail.com>
2025-01-26 22:21:24 -0600

    updated install.sh functions

--- qtile/install.sh
+++ qtile/install.sh
@@ -13,7 +13,7 @@ PACKAGES=(
   xorg
   xorg-server
   qtile
-  fwd
+  gufw
   network-manager-applet
   bluez
   bluez-utils
@@ -32,18 +32,52 @@ reset="\033[0m"
 
 # print messages with green color
 function success_message(){
-    echo -e "$(green)$1${reset}"
+  echo -e "$(green)$1${reset}"
 }
 
 if [[ $EUID -ne 0 ]]; then 
-    echo "This script have to be executed by root." >&2
-    exit 1
+  echo "This script have to be executed by root." >&2
+  exit 1
 fi
 
 # update the system
 success_message "Updating the system..."
-pacman -Syu --no confirm
+pacman -Syu --noconfirm
 
 # Install basic packages
 success_message "Installing basics packages.... ${PACKAGES[*]}...."
-pacman -S --noconfirm ${PACKAGES[*]}
+for paquete in "${PACKAGE[@]}"; do
+    if pacman -Qi "$paquete" &>/dev/null; then
+        success_message "$paquete ya está instalado."
+    else
+        if ! pacman -S --noconfirm "$paquete"; then
+            error_message "Error instaling $paquete...."
+        else
+            success_message "Packages installed successfully"
+        fi
+    fi
+done
+
+
+# yay
+if ! command -v yay &>/dev/null; then
+  success_message "Installing yay"
+  git clone https://aur.archlinux.org/yay.git /tmp/yay
+  cd /tmp/yay
+  makepkg -si --noconfirm
+  cd - && rm -rf /tmp/yay
+fi
+
+# clone git config
+success_message "cloning configuration from &CONFIG_REPO"
+if [[ -d "$CONFIG_DIR" ]]; then
+    mv "$CONFIG_DIR" "$CONFIG_DIR.bak"
+    success_message "Copia de seguridad de la configuración existente guardada como $CONFIG_DIR.bak"
+fi
+git clone "$CONFIG_REPO" "$CONFIG_DIR"
+
+# Cambiar permisos si es necesario
+chown -R "$USER:$USER" "$CONFIG_DIR"
+
+# Finalización
+success_message "Configuración de post-instalación completada con éxito."
